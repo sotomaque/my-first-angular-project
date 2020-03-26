2cea3dd9ade4dd2022a2953f817bdfd8ac03cdb8
sotomaque <sotomaque@gmail.com>
2020-03-26 16:45:27 -0700

    added a new component and class, impelmented first new view using ngfor to print a table

---
+++ src/app/sales-person-list/sales-person-list.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+import { SalesPerson } from './sales-person';
+
+@Component({
+  selector: 'app-sales-person-list',
+  templateUrl: './sales-person-list.component.html',
+  styleUrls: ['./sales-person-list.component.css']
+})
+export class SalesPersonListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  salesPersonList: SalesPerson[] = [
+    new SalesPerson("Anup", "Kumar", "anup.kumar@gmail.com", 50000),
+    new SalesPerson("James", "Bond", "james.bond@gmail.com", 50000),
+    new SalesPerson("Lindsey", "Kevin", "lindsey.kevin@gmail.com", 50000),
+    new SalesPerson("Juan", "Rosas", "juan.rosas@gmail.com", 50000)
+  ]
+
+}
