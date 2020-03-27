a0a8c8e0c91830fce870576addb5f675c5fff7e7
sotomaque <sotomaque@gmail.com>
2020-03-26 18:56:58 -0700

    added conditional ddirective

--- src/app/sales-person-list/sales-person-list.component.ts
+++ src/app/sales-person-list/sales-person-list.component.ts
@@ -3,7 +3,7 @@ import { SalesPerson } from './sales-person';
 
 @Component({
   selector: 'app-sales-person-list',
-  templateUrl: './sales-person-list.component.html',
+  templateUrl: './sales-person-list-bootstrap.component.html',
   styleUrls: ['./sales-person-list.component.css']
 })
 export class SalesPersonListComponent implements OnInit {
@@ -15,9 +15,9 @@ export class SalesPersonListComponent implements OnInit {
 
   salesPersonList: SalesPerson[] = [
     new SalesPerson("Anup", "Kumar", "anup.kumar@gmail.com", 50000),
-    new SalesPerson("James", "Bond", "james.bond@gmail.com", 50000),
-    new SalesPerson("Lindsey", "Kevin", "lindsey.kevin@gmail.com", 50000),
-    new SalesPerson("Juan", "Rosas", "juan.rosas@gmail.com", 50000)
+    new SalesPerson("James", "Bond", "james.bond@gmail.com", 40000),
+    new SalesPerson("Lindsey", "Kevin", "lindsey.kevin@gmail.com", 90000),
+    new SalesPerson("Juan", "Rosas", "juan.rosas@gmail.com", 1000)
   ]
 
 }
